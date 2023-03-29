a5ba91ae09a85ca38cf290afd8cd1658a4a9914b
SUNNY BHAVEEN CHANDRA <34603575+c17hawke@users.noreply.github.com>
2023-03-29 09:25:06 +0000

    open ai basic demo complete

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from dotenv import load_dotenv
 import os
+import openai
+
 load_dotenv()  # take environment variables from .env.
 
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
-print(OPENAI_API_KEY)
+prompt = input("Enter your prompt: ")
+
+response = openai.Completion.create(
+  model="text-davinci-003",
+  prompt=prompt,
+  max_tokens=100,
+  temperature=0
+)
+
+print(response["choices"][0]["text"])
